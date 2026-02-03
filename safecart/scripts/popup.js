6ff7f1bea958453ce7f577cf53443b57da93b442
IdlingCrow <hoangkienle37@gmail.com>
2026-02-02 21:59:25 -0800

    add all the collection of remaining element that needed to be collected for rudimentary.js. also updated the popup to display so.

--- safecart/scripts/popup.js
+++ safecart/scripts/popup.js
@@ -1,10 +1,16 @@
+//Check if this is a active tab, and that it is the current tab
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    //Ask the whole extension that is active in the tab to "getData"
     chrome.tabs.sendMessage(tabs[0].id, {action: "getData"}, (response) => {
         if(response) {
-
+            //changing the element of the popup.html based on the data recived
             document.getElementById("title").textContent = response.title;
             document.getElementById("rating").textContent = response.productRating;
+            document.getElementById("listingPrice").textContent = response.listingPrice;
+            document.getElementById("numSold").textContent = response.numSold;
 
+            //look throught the reivew list from the response and slowly append it to 
+            //the <ul> review-list 
             const reviewList = document.getElementById("reviews-list");
             const responseReview = response.reviews;
             reviewList.innerHTML = "";
--- safecart/scripts/scanner.js
+++ safecart/scripts/scanner.js
@@ -1,7 +1,7 @@
 let title;
 let productRating;
 let reviews;
-let price;
+let listingPrice;
 let numSold;
 
 function gatherReview() {
@@ -17,21 +17,27 @@ function gatherRating() {
 }
 
 function gatherPrice() {
-    price = document.querySelector('span[class="price-default--current--F8OlYIo"]');
+    listingPrice = document.querySelector('span[class="price-default--current--F8OlYIo"]');
 }
 
 function gatherNumSold() {
     numSold = document.querySelector('span[class="reviewer--sold--ytPeoEy"]');
 }
 
+//listening for any query
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
+    //check if the request is called "get data"
     if(request.action === "getData") {
+
         gatherReview();
         gatherTitle();
         gatherRating();
         gatherPrice();
+        gatherNumSold()
 
+        //creating a an array and extract all of the html from the reviews
+        //then put it in the array
         const reviewsArray = [];
         if(reviews && reviews.length > 0) {
             for(let i = 0; i < reviews.length; i++) {
@@ -39,11 +45,12 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
             }
         }
 
-        console.log(price.textContent);
-
+        //send all of the information called above and the extracted array
         sendResponse({title: title ? title.textContent : "no value yet", 
             reviews: reviewsArray, 
-            productRating: productRating ? productRating.textContent: 0});
+            productRating: productRating ? productRating.textContent: 0, 
+            listingPrice: listingPrice ? listingPrice.textContent: 0,
+            numSold: numSold ? numSold.textContent: 0});
     }
     return true;
 })
