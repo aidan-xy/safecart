288ac3e851b6dd4e27c1c9b83d4c362b430d6cfb
aidanyu <aidanxiaomoyu@gmail.com>
2026-02-12 14:28:06 -0800

    rewrite score tests

--- safecart/tests/trustScoring.test.js
+++ safecart/tests/trustScoring.test.js
@@ -19,14 +19,14 @@ describe('full alg tests', () => {
 describe('simple alg tests', () => {
   // "good" listings, expect above a certain score
   test('theoretical perfect listing', () => {
-    expect(simpleTrustAlg(5, 1000, 5, 1000, 1000)).toBeGreaterThanOrEqual(0.90);
+    expect(simpleTrustAlg(5, 1000, 5, 1000, 1000).score).toBeGreaterThanOrEqual(90);
   });
   test('excellent listing, Ajazz AK820 Mechanical Keyboard', () => {
-    expect(simpleTrustAlg(4.8, 10000, 2.993, 4661, 1223)).toBeGreaterThanOrEqual(0.90);
+    expect(simpleTrustAlg(4.8, 10000, 2.993, 4661, 1223).score).toBeGreaterThanOrEqual(90);
   });
 
   // "bad" listings, expect below a certain score
   test('clearly scam listing, New Original zotac 4090', () => {
-    expect(simpleTrustAlg(1.6, 34, 1.253, 7, 0)).toBeLessThanOrEqual(0.30);
+    expect(simpleTrustAlg(1.6, 34, 1.253, 7, 0).score).toBeLessThanOrEqual(30);
   });
 });
