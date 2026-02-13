39c65bacfe7f473da2b1cac44f28be4a4e55353f
IdlingCrow <hoangkienle37@gmail.com>
2026-02-12 17:08:44 -0800

    Made changes to package, scanner, and its test changes packages so we can use jest and chrome API changes scanner to typescript, and more function for simple AGI, added a listener to get data for simple AGI created some more test for scanner because of the more function

--- safecart/tests/parsing.test.js
+++ safecart/tests/parsing.test.js
@@ -1,20 +1,32 @@
 /**
  * @jest-environment jsdom
  */
-
-const { gatherTitle, gatherRating, gatherPrice, gatherNumSold, gatherReview } = require("../scripts/scanner");
+global.chrome = {
+  runtime: {    
+    onMessage: {
+      addListener: jest.fn()
+    }},
+};
+const {gatherNumberRatings, 
+        gatherTitle, 
+        gatherRating, 
+        gatherPrice, 
+        gatherNumSold, 
+        gatherReview, 
+        gatherNumberImage, 
+        gatherOpenSinceDate,
+        gatherAge} = require("../scripts/scanner");
 const path = require("path");
 const fs = require("fs")
-
+//testing the page = https://www.aliexpress.us/item/3256810322652074.html?gatewayAdapt=glo2usa4itemAdapt
 describe('parsingTest', () => {
-
-  const html = fs.readFileSync(path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),"utf-8");
-
-  test('parsing title correctly', () => {
-    const html = fs.readFileSync(
+  
+  const html = fs.readFileSync(
       path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),
       "utf-8"
     );
+
+  test('parsing title correctly', () => {
 
     document.documentElement.innerHTML = html;
     const title = gatherTitle();
@@ -24,48 +36,30 @@ describe('parsingTest', () => {
 
 
   test('parsing rating correctly', () => {
-    const html = fs.readFileSync(
-      path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),
-      "utf-8"
-    );
 
     document.documentElement.innerHTML = html;
 
     const rating = gatherRating();
-    expect(rating.trim()).toEqual("4.4");   
+    expect(rating).toEqual(4.4);   
   })
 
   test('parsing price correctly', () => {
-    const html = fs.readFileSync(
-      path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),
-      "utf-8"
-    );
 
     document.documentElement.innerHTML = html;
 
-
     const price = gatherPrice();
-    expect(price).toEqual("$32.27");  
+    expect(price).toEqual(32.27);  
   })
 
   test('parsing numberSold correctly', () => {
-    const html = fs.readFileSync(
-      path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),
-      "utf-8"
-    );
 
     document.documentElement.innerHTML = html;
 
     const numSold = gatherNumSold();
-    expect(numSold).toEqual("800+ sold");
+    expect(numSold).toEqual(800);
   })
 
   test('parsing reviews correctly', () => {
-    const html = fs.readFileSync(
-      path.resolve(__dirname, "localHTMLpage/SOYES XS19 Mini Smartphone 3.88in 2GB RAM 16GB ROM Android10.0 With Facial Recognition Dual SIM Standby 3G Network Small Phone.html"),
-      "utf-8"
-    );
-
     document.documentElement.innerHTML = html;
     const reviews = gatherReview();
     
@@ -80,4 +74,43 @@ describe('parsingTest', () => {
     }
   })
 
+  test('parsing number of images correctly correctly', () => {
+
+    document.documentElement.innerHTML = html;
+
+    const numSold = gatherNumberImage();
+    expect(numSold).toEqual(19);
+  })
+
+  test('parsing number of images correctly correctly', () => {
+
+    document.documentElement.innerHTML = html;
+
+    const numSold = gatherNumberImage();
+    expect(numSold).toEqual(19);
+  })
+
+  test('parsing number of Ratings correctly', () => {
+
+    document.documentElement.innerHTML = html;
+
+    const numRatings = gatherNumberRatings();
+    expect(numRatings).toEqual(131);
+  })
+  
+  test('parsing the open since date correctly', () => {
+
+    document.documentElement.innerHTML = html;
+
+    const date = gatherOpenSinceDate();
+    expect(date).toEqual("march, 22 2006");
+  })
+
+  test('parsing the age correctly', () => {
+
+    document.documentElement.innerHTML = html;
+
+    const age = gatherAge();
+    expect(age).toEqual(-1);
+  })
 });
