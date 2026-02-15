79e112ada672aba2df36b7d94e08f152f40cc066
Aidan Yu <aidanxiaomoyu@gmail.com>
2026-02-14 19:38:05 -0800

    update trust score tests

--- safecart/tests/trustScoring.test.js
+++ safecart/tests/trustScoring.test.js
@@ -4,15 +4,15 @@ const simpleTrustAlg = require("../scripts/simpleTrustAlg");
 describe('full alg tests', () => {
   // "good" listings, expect above a certain score
   test('theoretical perfect listing', () => {
-    expect(trustAlg(100, 100, 5, 1000, 5, 1000, 1000)).toBeGreaterThanOrEqual(0.90);
+    expect(trustAlg(100, 100, 5, 1000, 5, 1000, 1000).score).toBeGreaterThanOrEqual(90);
   });
   test('excellent listing, Ajazz AK820 Mechanical Keyboard', () => {
-    expect(trustAlg(76.73, 90, 4.8, 10000, 2.993, 4661, 1223)).toBeGreaterThanOrEqual(0.90);
+    expect(trustAlg(76.73, 90, 4.8, 10000, 2.993, 4661, 1223).score).toBeGreaterThanOrEqual(90);
   });
 
   // "bad" listings, expect below a certain score
   test('clearly scam listing, New Original zotac 4090', () => {
-    expect(trustAlg(500, 1600, 1.6, 34, 1.253, 7, 0)).toBeLessThanOrEqual(0.30);
+    expect(trustAlg(500, 1600, 1.6, 34, 1.253, 7, 0).score).toBeLessThanOrEqual(30);
   });
 });
 
