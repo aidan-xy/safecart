da63a5f22236122e4bb2788a33b02ace2cb76e73
IdlingCrow <hoangkienle37@gmail.com>
2026-02-17 18:04:12 -0800

    adding semi colon at the end  of each statment for better readability

--- safecart/scripts/scanner.js
+++ safecart/scripts/scanner.js
@@ -9,7 +9,7 @@ function gatherReview() {
     console.log("recording review " + (i + 1) + " : " + reviews[i].textContent);
     }
   }
-  return reviewsArray
+  return reviewsArray;
 }
 
 //Gathering and parsing the html for the title element
@@ -17,7 +17,7 @@ function gatherReview() {
 function gatherTitle() {
   const title = document.querySelector('h1[data-pl="product-title"]');
   console.log("Title element: " +  (title ? title.textContent : "no value yet"));
-  return title ? title.textContent : "no value yet"
+  return title ? title.textContent : "no value yet";
 }
 
 //Gathering and parsing the html for the rating element
@@ -25,12 +25,12 @@ function gatherTitle() {
 function gatherRating() {
   const productRatingHTML = document.querySelector('a[class="reviewer--rating--xrWWFzx"] strong');
   if(!productRatingHTML){
-    console.log("could not see rating element")
-    return 0
+    console.log("could not see rating element");
+    return 0;
   }
-  const productRating = productRatingHTML.textContent.match(/\d{1}.\d{1}/)
+  const productRating = productRatingHTML.textContent.match(/\d{1}.\d{1}/);
   console.log("Rating element: " +  (productRating ? parseFloat(productRating [0]): 0));
-  return productRating ? parseFloat(productRating [0]): 0
+  return productRating ? parseFloat(productRating [0]): 0;
 }
 
 //Gathering and parsing the html for the number sold element
@@ -38,12 +38,12 @@ function gatherRating() {
 function gatherPrice() {
   const listingPrice = document.querySelector('span[class="price-default--current--F8OlYIo"]');
   if(!listingPrice){
-    console.log("could not see product price element")
-    return 0
+    console.log("could not see product price element");
+    return 0;
   }
-  const match = listingPrice.textContent.match(/[\d,]+\.\d{2}/)
+  const match = listingPrice.textContent.match(/[\d,]+\.\d{2}/);
   console.log("Product price element: " + (match ? parseFloat(match[0]): 0));
-  return match ? parseFloat(match[0].replace(/,/g, '')): 0
+  return match ? parseFloat(match[0].replace(/,/g, '')): 0;
 }
 
 //Gathering and parsing the html for the number sold element
@@ -51,12 +51,12 @@ function gatherPrice() {
 function gatherNumSold() {
   const numSoldHTML = document.querySelector('span[class="reviewer--sold--ytPeoEy"]');
   if(!numSoldHTML){
-    console.log("could not see num sold element")
-    return 0
+    console.log("could not see num sold element");
+    return 0;
   }
   const numSold = numSoldHTML.textContent.match(/[\d,]+/)
   console.log("Num sold element: " + (numSold ? parseInt(numSold[0].replace(/,/g, '')): 0));
-  return numSold ? parseInt(numSold[0].replace(/,/g, '')): 0 
+  return numSold ? parseInt(numSold[0].replace(/,/g, '')): 0;
 }
 
 //Gathering and parsing the html for the number of images element
@@ -64,11 +64,11 @@ function gatherNumSold() {
 function gatherNumberImage() {
   const numberImageImage = document.querySelector('span[class="comet-icon comet-icon-photo filter--labelIcon--O0LEQIg"]');
   if(!numberImageImage){
-    console.log("could not see number of images element")
-    return 0
+    console.log("could not see number of images element");
+    return 0;
   }
-  const numberImageImageParent = numberImageImage.parentElement
-  const numberImage = numberImageImageParent.textContent.match(/\d+/)
+  const numberImageImageParent = numberImageImage.parentElement;
+  const numberImage = numberImageImageParent.textContent.match(/\d+/);
   console.log("Number of images element: " + (numberImage ? parseInt(numberImage[0]) : 0));
   return numberImage ? parseInt(numberImage[0]) : 0;
 }
@@ -78,10 +78,10 @@ function gatherNumberImage() {
 function gatherNumberRatings() {
   const numberOfRatingsHTML = document.querySelector('a[class="reviewer--reviews--cx7Zs_V"]');
   if(!numberOfRatingsHTML){
-    console.log("could not see number of ratings element")
-    return 0
+    console.log("could not see number of ratings element");
+    return 0;
   }
-  const numberOfRatings = numberOfRatingsHTML.textContent.match(/\d+/)
+  const numberOfRatings = numberOfRatingsHTML.textContent.match(/\d+/);
   console.log("Number of ratings element: " + (numberOfRatings ? parseFloat(numberOfRatings[0]) : 0));
   return numberOfRatings ? parseFloat(numberOfRatings[0]) : 0;
 }
@@ -95,15 +95,15 @@ function gatherOpenSinceDate() {
     const eachInfo = ageHTML.querySelectorAll('td');
     for (let i = 0; i < eachInfo.length; i++) {
       if (eachInfo[i].textContent.trim() === 'Open since:' && eachInfo[i + 1].textContent.trim() != "") {
-        date = eachInfo[i + 1]
-        break
+        date = eachInfo[i + 1];
+        break;
       }
     }
     console.log("Open since date element: " +  (date ? date.textContent.trim() : ""));
     return date ? date.textContent.trim() : "";
   } else{
     console.log("Could not see open since date element");
-    return ""
+    return "";
   }
 
 }
@@ -118,13 +118,13 @@ function gatherAge() {
     const targetDate = new Date(dateStr);
     const today = new Date();
     age = ((today.getTime() - targetDate.getTime()) / (1000 * 60 * 60 * 24 * 365.25));
-    age = Math.round(age * 100)/100
+    age = Math.round(age * 100)/100;
     console.log("caluclated age :" + age);
   } else {
     console.log("could not caluclate age");
-    age = 0
+    age = 0;
   }
-  return age
+  return age;
 }
 
 //getting all the information for the simpleTrustAGI() class
@@ -134,9 +134,9 @@ function getAllInformationForSimpleAGI() {
                             numSold: gatherNumSold(), 
                             ageYears: gatherAge(),
                             numRating: gatherNumberRatings(),
-                            reviewImages: gatherNumberImage()}
+                            reviewImages: gatherNumberImage()};
   
-  return infoForSimpleAGI
+  return infoForSimpleAGI;
 }
 
 
